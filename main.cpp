f0bb1b68e457883a4e3b2b1ca5781778e5f53fc7
Guilherme Oliveira <gui.r.oliveira.88@gmail.com>
2022-01-23 19:48:01 -0300

    Ajuste nos dados da solução

--- main.cpp
+++ main.cpp
@@ -6,21 +6,25 @@
 
 #include <iostream>
 #include <string>
+#define LENGTH(x) (sizeof(x) / sizeof(x)[0])
 
 using namespace std;
 int main() {
+  string instancia = "txt\\darp1.txt";
+  lerArquivo(instancia);
   Solucao s;
-  lerArquivo("txt\\darp1.txt", s);
-  escreverDados("", s);
-  // calcularFO();
+  HCAleatoria(s);
+  calcularFO(s);
+  string arquivoSaida = "";
+  escreverDados(arquivoSaida);
+  // calcularFO(s);
 }
 
-void lerArquivo(string nome, Solucao &s) {
+void lerArquivo(string nome) {
   FILE *f = fopen(nome.c_str(), "r");
-  fscanf(f, "%d %d %d %d %d", &s.entrada.veiculos, &s.entrada.requisicoes, &s.entrada.duracaoRotaMax,
-         &s.entrada.tempoViagemMax, &s.entrada.tempoEsperaMax);
-  int requisicaoTotal = s.entrada.requisicoes * 2 + 2;
-  for (int i = 0; i < s.entrada.veiculos; i++) {
+  fscanf(f, "%d %d %d %d %d", &veiculos, &requisicoes, &duracaoRotaMax, &tempoViagemMax, &tempoEsperaMax);
+  int requisicaoTotal = requisicoes * 2 + 2;
+  for (int i = 0; i < veiculos; i++) {
     fscanf(f, "%d", &capVeiculos[i]);
   }
   for (int i = 0; i < requisicaoTotal; i++) {
@@ -37,46 +41,49 @@ void lerArquivo(string nome, Solucao &s) {
   }
   for (int i = 0; i < requisicaoTotal; i++) {
     for (int j = 0; j < requisicaoTotal; j++) {
-      fscanf(f, "%d", &matriz[i][j]);
+      fscanf(f, "%d", &matrizTempoDeslocamento[i][j]);
     }
+  }
+  for (int i = 0; i < requisicaoTotal; i++) {
+    janelaTempoTotal[i] = fimJanelaTempo[i] - inicioJanelaTempo[i];
   }
   fclose(f);
 }
 
-void escreverDados(string nome, Solucao &s) {
+void escreverDados(string nome) {
   FILE *f;
   if (nome == "") {
     f = stdout;
   } else {
     f = fopen(nome.c_str(), "w");
   }
-  fprintf(f, "%d\t%d\t%d\t%d\t%d\n", s.entrada.veiculos, s.entrada.requisicoes,
-          s.entrada.duracaoRotaMax, s.entrada.tempoViagemMax, s.entrada.tempoEsperaMax);
-  int totalRequisicoes = s.entrada.requisicoes * 2 + 2;
-  breakLine(f, 5);
-  for (int i = 0; i < s.entrada.veiculos; i++) {
+  fprintf(f, "%d\t%d\t%d\t%d\t%d\n", veiculos, requisicoes,
+          duracaoRotaMax, tempoViagemMax, tempoEsperaMax);
+  int totalRequisicoes = requisicoes * 2 + 2;
+  breakLine(f, 3);
+  for (int i = 0; i < veiculos; i++) {
     fprintf(f, "%d\t", capVeiculos[i]);
   }
-  breakLine(f, 5);
+  breakLine(f, 3);
   for (int i = 0; i < totalRequisicoes; i++) {
     fprintf(f, "%d\t", numAssentos[i]);
   }
-  breakLine(f, 5);
+  breakLine(f, 3);
   for (int i = 0; i < totalRequisicoes; i++) {
     fprintf(f, "%d\t", tempoServico[i]);
   }
-  breakLine(f, 5);
+  breakLine(f, 3);
   for (int i = 0; i < totalRequisicoes; i++) {
     fprintf(f, "%d\t", inicioJanelaTempo[i]);
   }
-  breakLine(f, 5);
+  breakLine(f, 3);
   for (int i = 0; i < totalRequisicoes; i++) {
     fprintf(f, "%d\t", fimJanelaTempo[i]);
   }
-  breakLine(f, 5);
+  breakLine(f, 3);
   for (int i = 0; i < totalRequisicoes; i++) {
     for (int j = 0; j < totalRequisicoes; j++) {
-      fprintf(f, "%d\t", matriz[i][j]);
+      fprintf(f, "%d\t", matrizTempoDeslocamento[i][j]);
     }
     breakLine(f, 3);
   }
@@ -89,19 +96,44 @@ void simulatedAnnealing() {
   fim = clock();
 }
 
-void HCAleatoria(Solucao &s) {}
+void HCAleatoria(Solucao &s) {
+}
 
-void HCGulosa(Solucao &s) {}
+void HCGulosa(Solucao &s) {
+}
 
 void HCAleatoriaGulosa(Solucao &s) {}
 
 void calcularFO(Solucao &s) {
-  int p1 = PESO1 * s.entrada.veiculos;
+  int p1 = PESO1 * veiculos;
   int p2 = 0;
+  for (int i = 0; i < veiculos; i++) {
+  }
+  // Iterando
+  for (int i = 1; i < requisicoes * 2 + 1; i++) {
+    int embarque = i;
+    int desembarque = i + 1;
+  }
 }
 
 void clonarSolucao(Solucao &original, Solucao &copia) {
   memcpy(&copia, &original, sizeof(original));
+}
+
+void escreverSolucao(Solucao &s, const bool flag) {
+  printf("FO: %d\n", s.FO);
+  // if (flag) {
+  //   printf("Veiculos usados: %d\n");
+  //   printf("Distancia percorrida: %d\n");
+  //   printf("Duração das rotas: %d\n");
+  //   printf("Tempo total de viagem: %d\n");
+  //   printf("Tempo total de espera: %d\n");
+  //   breakLine(stdout, 3);
+  //   printf("Violações de assentos: %d\n");
+  //   printf("Violações da duração maxima de rotas: %d\n");
+  //   printf("Violações do tempo maximo de espera: %d\n");
+  //   printf("Violações dos horarios de saida e chegada: %d\n");
+  // }
 }
 
 void breakLine(FILE *f, const int num) {
--- main.hpp
+++ main.hpp
@@ -2,38 +2,58 @@
 #include <string>
 
 #define MAX 1440
+#define VEICULOS 6
 
 // Dados de entrada
-typedef struct Entrada {
-  int veiculos, requisicoes, duracaoRotaMax;
-  int tempoViagemMax, tempoEsperaMax;
-} Entrada;
-
-typedef struct Solucao {
-  int idVeiculoTransportado[MAX];  // 0= nenhum,1,2,3,4,5,6
-  int pessoasTransportadas[MAX];
-  int tempoTotalMovimento[MAX];
-  int FO;
-  Entrada entrada;
-} Solucao;
-
-// int veiculos, requisicoes, duracaoRotaMax, tempoViagemMax, tempoEsperaMax;
-
-int capVeiculos[MAX],
+int veiculos, requisicoes, duracaoRotaMax, tempoViagemMax, tempoEsperaMax,
+    capVeiculos[MAX],
     numAssentos[MAX],
     tempoServico[MAX],
     inicioJanelaTempo[MAX],
     fimJanelaTempo[MAX],
-    matriz[MAX][MAX];
+    janelaTempoTotal[MAX],
+    matrizTempoDeslocamento[MAX][MAX];
+
+// Estruturas de dados
+typedef struct Violacoes {
+  int numAssentos;
+  int duracaoMaximaRota;
+  int tempoMaximoViagem;
+  int tempoMaximoEspera;
+  int horarioSaidaEChegadaGaragens;
+} Violacoes;
+
+typedef struct Veiculo {
+  int id;
+  int requicoesAtendidas;
+  int assentosUtilizados;
+  int duracaoRota;
+  int tempoViagem;
+  int tempoEspera;
+  int distanciaPercorrida;
+  int FO;
+  Violacoes violacoes;
+  // TODO: sequência de locais que formam a rota do veículo.
+} Veiculo;
+
+typedef struct Solucao {
+  // int idVeiculoTransportado[MAX];  // -1= nenhum, 1,2,3,4,5,6
+  // int pessoasTransportadas[MAX];
+  // int tempoTotalMovimento[MAX];
+  // int requisicaoAtendidaPor[MAX];
+  Veiculo veiculos[VEICULOS];
+  int FO;
+} Solucao;
+
 int PESO1 = 10, PESO2 = 1;
 
-void lerArquivo(std::string nome, Solucao &s);
-void escreverDados(std::string nome, Solucao &s);
+void lerArquivo(std::string nome);
+void escreverDados(std::string nome);
 void HCAleatoria(Solucao &s);
 void HCGulosa(Solucao &s);
 void HCAleatoriaGulosa(Solucao &s);
 void calcularFO(Solucao &s);
 void clonarSolucao(Solucao &original, Solucao &copia);
+void escreverSolucao(Solucao &s);
 
-void simulatedAnnealing();
 void breakLine(FILE *f, const int num);
